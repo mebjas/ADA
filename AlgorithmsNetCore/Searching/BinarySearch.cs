40b44c5d01a32dcf4525bf0a7c45f47ed4e0b13b
minhaz <minhazv@microsoft.com>
2018-09-24 20:57:30 +0530

    Added binary search and binary search next greatest

---
+++ AlgorithmsNetCore/Searching/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public partial class Search : Helper
+    {
+        private static Func<int, int, int> DefaultComparator = (a, b) =>
+        {
+            return (a > b) ? 1 : -1;
+        };
+
+        public static int BinarySearch(int[] haystack, int needle, Func<int, int, int> comparator = null)
+        {
+            return BinarySearch(haystack, needle, 0, haystack.Length - 1, (comparator == null) ? DefaultComparator : comparator);
+        }
+
+        private static int BinarySearch(int[] haystack, int needle, int low, int high, Func<int, int, int> comparator)
+        {
+            int middle = (low + high) / 2;
+            if (haystack[middle] == needle)
+            {
+                return middle;
+            }
+
+            if (middle == low && middle == high)
+            {
+                return -1;
+            }
+
+            if (comparator(haystack[middle], needle) > 0)
+            {
+                return BinarySearch(haystack, needle, low, middle - 1, comparator);
+            }
+            else
+            {
+                return BinarySearch(haystack, needle, middle + 1, high, comparator);
+            }
+        }
+    }
+}
---
+++ AlgorithmsNetCore/Searching/BinarySearchNextGreatest.cs
@@ -0,0 +1,38 @@
+﻿namespace Algorithms
+{
+    public partial class Search
+    {
+        public static int BinarySearchNextGreatest(int[] arr, int needle)
+        {
+            return BinarySearchNextGreatest(arr, needle, 0, arr.Length - 1) + 1;
+        }
+
+        private static int BinarySearchNextGreatest(int[] haystack, int needle, int low, int high)
+        {
+            int middle = (low + high) / 2;
+            if (haystack[middle] == needle)
+            {
+                return middle;
+            }
+
+            if (needle < haystack[middle])
+            {
+                if (middle == high || haystack[middle + 1] < needle)
+                {
+                    return middle + 1;
+                }
+
+                return BinarySearchNextGreatest(haystack, needle, middle + 1, high);
+            }
+            else
+            {
+                if (middle == low || haystack[middle - 1] > needle)
+                {
+                    return middle;
+                }
+
+                return BinarySearchNextGreatest(haystack, needle, low, middle - 1);
+            }
+        }
+    }
+}
